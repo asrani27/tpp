a55cbe0d39880c02a6e1a0ccba2982e158fb815d
asrani <asrani.27@gmail.com>
2021-06-11 10:30:26 +0800

    fix

--- resources/views/pegawai/profil.blade.php
+++ resources/views/pegawai/profil.blade.php
@@ -17,7 +17,7 @@
             <img class="profile-user-img img-fluid img-circle" src="https://p.kindpng.com/picc/s/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png" alt="User profile picture">
           </div>
 
-          <h3 class="profile-username text-center">{{$daata->nama}}</h3>
+          <h3 class="profile-username text-center">{{$data->nama}}</h3>
 
           <p class="text-muted text-center">NIP. {{$data->nip}}</p>
 

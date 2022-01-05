7f806f899f3f9d80c19472c810030b742f66ea53
asrani <asrani.27@gmail.com>
2022-01-06 00:36:32 +0800

    f

--- resources/views/pegawai/skp/detail.blade.php
+++ resources/views/pegawai/skp/detail.blade.php
@@ -195,12 +195,12 @@ PEGAWAI
 
 @push('js')
 <script>
-  // function hanyaAngka(event) {
-  //       var angka = (event.which) ? event.which : event.keyCode
-  //       if (angka != 46 && angka > 31 && (angka < 48 || angka > 57))
-  //         return false;
-  //       return true;
-  //     }
+  function hanyaAngka(event) {
+        var angka = (event.which) ? event.which : event.keyCode
+        if (angka != 46 && angka > 31 && (angka < 48 || angka > 57))
+          return false;
+        return true;
+      }
 </script>
 
 @endpush

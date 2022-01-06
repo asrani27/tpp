232764ab3e2b1b2f32d8c2ad1a09f0cc8337fd45
asrani <asrani.27@gmail.com>
2022-01-06 08:00:18 +0800

    f

--- resources/views/pegawai/aktivitas/create.blade.php
+++ resources/views/pegawai/aktivitas/create.blade.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="/theme/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
 <!-- Ionicons -->
 <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    
+
 <!-- Timepicker -->
 <script src="/theme/plugins/moment/moment.min.js"></script>
 <link rel="stylesheet" href="/theme/plugins/daterangepicker/daterangepicker.css">
@@ -14,94 +14,101 @@
 @endpush
 
 @section('title')
-    TAMBAH AKTIVITAS HARIAN
+TAMBAH AKTIVITAS HARIAN
 @endsection
 @section('content')
 <div class="row">
-    <div class="col-md-12">
-      <a href="/pegawai/aktivitas/harian" class="btn btn-sm btn-secondary"><i class="fas fa-arrow-alt-circle-left"></i> Kembali</a><br/><br/>
-      <div class="card card-info">
-        <div class="card-header">
+  <div class="col-md-12">
+    <a href="/pegawai/aktivitas/harian" class="btn btn-sm btn-secondary"><i class="fas fa-arrow-alt-circle-left"></i>
+      Kembali</a><br /><br />
+    <div class="card card-info">
+      <div class="card-header">
         <h3 class="card-title"><i class="fas fa-graduation-cap"></i> Tambah Aktivitas</h3>
-        </div>
-        <!-- form start -->
-        <form id="aktivitas" class="form-horizontal" method="POST" action="/pegawai/aktivitas/add">
+      </div>
+      <!-- form start -->
+      <form id="aktivitas" class="form-horizontal" method="POST" action="/pegawai/aktivitas/add">
         @csrf
-          <div class="card-body">
-              <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Tanggal</label>
-                <div class="col-sm-10">
-                  
-                  <input type="date" class="form-control" name="tanggal" placeholder="" value="{{$tanggal}}"  min="{{\Carbon\Carbon::today()->subdays(2)->format('Y-m-d')}}" max="{{\Carbon\Carbon::today()->format('Y-m-d')}}">
-                </div>
-              </div>
-              <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Kegiatan</label>
-                <div class="col-sm-10">
-                  <select name="skp_id" class="form-control select2" required>
-                    <option value="">-kegiatan-</option>
-                    @foreach ($skp as $item)
-                    <option value="{{$item->id}}" {{$item->id == old('skp_id') ? 'selected':''}}>{{$item->deskripsi}}</option>
-                    @endforeach
-                  </select>
-                </div>
-              </div>
-              <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Nama Aktivitas</label>
-                <div class="col-sm-10">
-                  <input type="text" class="form-control" name="deskripsi" placeholder="Nama Aktivitas" value="{{old('deskripsi')}}">
-                </div>
-              </div>
-              <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Jam Mulai</label>
+        <div class="card-body">
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Tanggal</label>
+            <div class="col-sm-10">
 
-                <div class="col-sm-2">
-                <div class="input-group date" id="timepicker" data-target-input="nearest">
-                  <input type="text" class="form-control datetimepicker-input" data-target="#timepicker" name="jam_mulai" required value="{{$jam_mulai}}">
-                  <div class="input-group-append" data-target="#timepicker" data-toggle="datetimepicker">
-                      <div class="input-group-text"><i class="far fa-clock"></i></div>
-                  </div>
-                  </div>
-                </div>
-                <!-- /.input group -->
-              </div>
-              
-              <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Jam Selesai</label>
+              <input type="date" class="form-control" name="tanggal" placeholder="" value="{{$tanggal}}"
+                min="{{\Carbon\Carbon::today()->subdays(5)->format('Y-m-d')}}"
+                max="{{\Carbon\Carbon::today()->format('Y-m-d')}}">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Kegiatan</label>
+            <div class="col-sm-10">
+              <select name="skp_id" class="form-control select2" required>
+                <option value="">-kegiatan-</option>
+                @foreach ($skp as $item)
+                <option value="{{$item->id}}" {{$item->id == old('skp_id') ? 'selected':''}}>{{$item->deskripsi}}
+                </option>
+                @endforeach
+              </select>
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Nama Aktivitas</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" name="deskripsi" placeholder="Nama Aktivitas"
+                value="{{old('deskripsi')}}">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Jam Mulai</label>
 
-                <div class="col-sm-2">
-                <div class="input-group date" id="timepicker2" data-target-input="nearest">
-                  <input type="text" class="form-control datetimepicker-input" data-target="#timepicker2" name="jam_selesai" required value="{{$jam_selesai}}">
-                  <div class="input-group-append" data-target="#timepicker2" data-toggle="datetimepicker">
-                      <div class="input-group-text"><i class="far fa-clock"></i></div>
-                  </div>
-                  </div>
-                </div>
-                <!-- /.input group -->
-              </div>
-              
-              <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Dokumen Pendukung</label>
-                <div class="col-sm-10 custom-file">
-                    <input type="file" class="custom-file-input" name="file" id="customFile" disabled>
-                    <label class="custom-file-label" for="customFile">Choose file</label>
+            <div class="col-sm-2">
+              <div class="input-group date" id="timepicker" data-target-input="nearest">
+                <input type="text" class="form-control datetimepicker-input" data-target="#timepicker" name="jam_mulai"
+                  required value="{{$jam_mulai}}">
+                <div class="input-group-append" data-target="#timepicker" data-toggle="datetimepicker">
+                  <div class="input-group-text"><i class="far fa-clock"></i></div>
                 </div>
               </div>
+            </div>
+            <!-- /.input group -->
           </div>
-          
-          <div class="card-footer">
-              <button type="submit" class="btn btn-block btn-info btnSubmit"><i class="fas fa-save"></i> Simpan</button>
+
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Jam Selesai</label>
+
+            <div class="col-sm-2">
+              <div class="input-group date" id="timepicker2" data-target-input="nearest">
+                <input type="text" class="form-control datetimepicker-input" data-target="#timepicker2"
+                  name="jam_selesai" required value="{{$jam_selesai}}">
+                <div class="input-group-append" data-target="#timepicker2" data-toggle="datetimepicker">
+                  <div class="input-group-text"><i class="far fa-clock"></i></div>
+                </div>
+              </div>
+            </div>
+            <!-- /.input group -->
           </div>
-        </form>
+
+          <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Dokumen Pendukung</label>
+            <div class="col-sm-10 custom-file">
+              <input type="file" class="custom-file-input" name="file" id="customFile" disabled>
+              <label class="custom-file-label" for="customFile">Choose file</label>
+            </div>
+          </div>
+        </div>
+
+        <div class="card-footer">
+          <button type="submit" class="btn btn-block btn-info btnSubmit"><i class="fas fa-save"></i> Simpan</button>
+        </div>
+      </form>
     </div>
-    
-    </div>
+
+  </div>
 </div>
 @endsection
 
 @push('js')
 <script>
-    $(document).ready(function () {
+  $(document).ready(function () {
         $("#aktivitas").submit(function () {
         $(".btnSubmit").attr("disabled", true);
         return true;
@@ -117,7 +124,7 @@
 <!-- Select2 -->
 <script src="/theme/plugins/select2/js/select2.full.min.js"></script>
 <script>
-    $(function () {
+  $(function () {
       //Initialize Select2 Elements
       $('.select2').select2()
   
@@ -141,7 +148,7 @@
 <script src="/theme/plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
 
 <script type="text/javascript">
-    $(document).ready(function () {
+  $(document).ready(function () {
       bsCustomFileInput.init();
     });
 </script>

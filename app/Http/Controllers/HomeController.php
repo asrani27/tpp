3355fd5074bedbc4fbcce3120d9a30bd294ac1e0
asrani <asrani.27@gmail.com>
2022-01-25 16:36:11 +0800

    f

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -238,7 +238,8 @@ class HomeController extends Controller
                 $item->tambahan_persen_tpp = $item->jabatan->tambahan_persen_tpp;
                 $item->jumlah_persentase = $persentase_tpp + $item->jabatan->tambahan_persen_tpp;
                 $item->total_pagu = ceil($item->basic_tpp * ($persentase_tpp + $item->tambahan_persen_tpp) / 100);
-                $item->persen_disiplin = Presensi::where('pegawai_id', $item->id)->where('bulan', $month)->where('tahun', $year)->first() == null ? 0 : Presensi::where('pegawai_id', $item->id)->where('bulan', $month)->where('tahun', $year)->first()->persen;
+                $presensi = Presensi::where('pegawai_id', $item->id)->where('bulan', $month)->where('tahun', $year)->first();
+                $item->persen_disiplin =  0;
                 $item->total_disiplin =  $item->total_pagu * 40 / 100;
                 $item->persen_produktivitas = 100;
                 if ($jmlmenit >= 6750) {

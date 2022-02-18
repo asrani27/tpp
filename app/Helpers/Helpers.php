8043d0a6b538197a5879d9f9ad45292f1adf4f73
asrani <asrani.27@gmail.com>
2022-02-18 14:13:13 +0800

    d

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -8,115 +8,114 @@ use App\Pangkat;
 use App\Pegawai;
 use App\Parameter;
 
-function terbilang($angka) {
-    $angka=abs($angka);
-    $baca =array("", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas");
-  
-    $terbilang="";
-     if ($angka < 12){
-         $terbilang= " " . $baca[$angka];
-     }
-     else if ($angka < 20){
-         $terbilang= terbilang($angka - 10) . " belas";
-     }
-     else if ($angka < 100){
-         $terbilang= terbilang($angka / 10) . " puluh" . terbilang($angka % 10);
-     }
-     else if ($angka < 200){
-         $terbilang= " seratus" . terbilang($angka - 100);
-     }
-     else if ($angka < 1000){
-         $terbilang= terbilang($angka / 100) . " ratus" . terbilang($angka % 100);
-     }
-     else if ($angka < 2000){
-         $terbilang= " seribu" . terbilang($angka - 1000);
-     }
-     else if ($angka < 1000000){
-         $terbilang= terbilang($angka / 1000) . " ribu" . terbilang($angka % 1000);
-     }
-     else if ($angka < 1000000000){
-        $terbilang= terbilang($angka / 1000000) . " juta" . terbilang($angka % 1000000);
-     }
-        return $terbilang;
- }
+function terbilang($angka)
+{
+    $angka = abs($angka);
+    $baca = array("", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas");
 
- function skpd()
- {
-     return Skpd::get();
- }
+    $terbilang = "";
+    if ($angka < 12) {
+        $terbilang = " " . $baca[$angka];
+    } else if ($angka < 20) {
+        $terbilang = terbilang($angka - 10) . " belas";
+    } else if ($angka < 100) {
+        $terbilang = terbilang($angka / 10) . " puluh" . terbilang($angka % 10);
+    } else if ($angka < 200) {
+        $terbilang = " seratus" . terbilang($angka - 100);
+    } else if ($angka < 1000) {
+        $terbilang = terbilang($angka / 100) . " ratus" . terbilang($angka % 100);
+    } else if ($angka < 2000) {
+        $terbilang = " seribu" . terbilang($angka - 1000);
+    } else if ($angka < 1000000) {
+        $terbilang = terbilang($angka / 1000) . " ribu" . terbilang($angka % 1000);
+    } else if ($angka < 1000000000) {
+        $terbilang = terbilang($angka / 1000000) . " juta" . terbilang($angka % 1000000);
+    }
+    return $terbilang;
+}
 
- function countSkpd()
- {
-     return Skpd::get()->count();
- }
+function skpd()
+{
+    return Skpd::get();
+}
 
- function countPegawai()
- {
-     return Pegawai::get()->count();
- }
- 
- function countPegawaiSkpd($id)
- {
-     return Pegawai::where('skpd_id', $id)->get()->count();
- }
+function countSkpd()
+{
+    return Skpd::get()->count();
+}
 
- function countJabatanSkpd($id)
- {
+function countPegawai()
+{
+    return Pegawai::get()->count();
+}
+
+function countPegawaiSkpd($id)
+{
+    return Pegawai::where('skpd_id', $id)->get()->count();
+}
+
+function countJabatanSkpd($id)
+{
     return Jabatan::where('skpd_id', $id)->get()->count();
- }
+}
 
- function grafikSkpd()
- {
-     return Skpd::get()->pluck('kode_skpd');
- }
+function grafikSkpd()
+{
+    return Skpd::get()->pluck('kode_skpd');
+}
 
- function pangkat()
- {
-     return Pangkat::get();
- }
+function pangkat()
+{
+    return Pangkat::get();
+}
 
- function eselon()
- {
-     return Eselon::get();
- }
- 
- function kelas()
- {
-     return Kelas::orderBy('id','DESC')->get();
- }
- function pegawai()
- {      
-     return Pegawai::orderBy('nama','ASC')->paginate(10);
- }
+function eselon()
+{
+    return Eselon::get();
+}
 
- function detailSkpd($id)
- {
-     return Skpd::find($id);
- }
+function kelas()
+{
+    return Kelas::orderBy('id', 'DESC')->get();
+}
+function pegawai()
+{
+    return Pegawai::orderBy('nama', 'ASC')->paginate(10);
+}
 
- function jabatan($skpd_id)
- {
+function detailSkpd($id)
+{
+    return Skpd::find($id);
+}
+
+function jabatan($skpd_id)
+{
     $data = Jabatan::where('skpd_id', $skpd_id)->get();
     return $data;
- }
+}
 
- function pegawaiSkpd($id)
- {
-     return Pegawai::where('skpd_id', $id)->paginate(10);
- }
+function pegawaiSkpd($id)
+{
+    return Pegawai::where('skpd_id', $id)->paginate(10);
+}
 
- function parameter()
- {
-     return Parameter::get();
- }
+function parameter()
+{
+    return Parameter::get();
+}
 
- function currency($value)
- {
-    $hasil=number_format($value,0,',','.');
+function currency($value)
+{
+    $hasil = number_format($value, 0, ',', '.');
     return $hasil;
- }
- 
- function persentase_tpp()
- {
-     return Parameter::first()->persentase_tpp;
- }
+}
+
+function persentase_tpp()
+{
+    return Parameter::first()->persentase_tpp;
+}
+
+function bulanTahun()
+{
+    return BulanTahun::orderBy('id', 'DESC')->get();
+}
--- resources/views/pegawai/laporan/aktivitas.blade.php
+++ resources/views/pegawai/laporan/aktivitas.blade.php
@@ -1,82 +1,109 @@
 @extends('layouts.app')
 
 @push('css')
-  <link rel="stylesheet" href="/theme/plugins/select2/css/select2.min.css">
-  <link rel="stylesheet" href="/theme/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<link rel="stylesheet" href="/theme/plugins/select2/css/select2.min.css">
+<link rel="stylesheet" href="/theme/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+<!-- Ionicons -->
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 @endpush
 @section('title')
-    LAPORAN AKTIVITAS
+LAPORAN AKTIVITAS
 @endsection
 @section('content')
 <div class="row">
-    <div class="col-md-12">
-        <h4>LAPORAN AKTIVITAS</h4>
-        <div class="row">
-            <div class="col-12">
-              <div class="card">
-                <div class="card-header">
-                  <h3 class="card-title">Total : {{count($data)}} Aktivitas</h3>
-  
-                  <div class="card-tools">
-                    <form method="get" action="/pegawai/skp/rencana-kegiatan/search">
-                    <div class="input-group input-group-sm" style="width: 300px;">
-                      <input type="text" name="search" class="form-control input-sm float-right" value="{{old('search')}}" placeholder="Cari">
-  
-                      <div class="input-group-append">
-                        <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
-                      </div>
-                    </div>
-                    </form>
+  <div class="col-md-12">
+    <h4>LAPORAN AKTIVITAS</h4>
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title"> Aktivitas</h3>
+
+            <div class="card-tools">
+              {{-- <form method="get" action="/pegawai/skp/rencana-kegiatan/search">
+                <div class="input-group input-group-sm" style="width: 300px;">
+                  <input type="text" name="search" class="form-control input-sm float-right" value="{{old('search')}}"
+                    placeholder="Cari">
+
+                  <div class="input-group-append">
+                    <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
                   </div>
                 </div>
-                <!-- /.card-header -->
-                <div class="card-body table-responsive p-0">
-                  <table class="table table-hover text-nowrap table-sm">
-                    <thead>
-                      <tr>
-                        <th>#</th>
-                        <th>Tanggal</th>
-                        <th>Jam</th>
-                        <th>Menit</th>
-                        <th>Aktivitas</th>
-                        <th>Status</th>
-                      </tr>
-                    </thead>
-                    @php
-                        $no =1;
-                    @endphp
-                    <tbody>
-                    @foreach ($data as $key => $item)
-                          <tr style="font-size:12px; font-family:Arial, Helvetica, sans-serif">
-                            <td>{{$key+ $data->firstItem()}}</td>
-                            <td>{{\Carbon\Carbon::parse($item->tanggal)->isoFormat('D MMMM Y')}}</td>
-                            <td>{{\Carbon\Carbon::parse($item->jam_mulai)->format('H:i')}} - {{\Carbon\Carbon::parse($item->jam_selesai)->format('H:i')}}</td>
-                            <td>{{$item->menit}}</td>
-                            <td>{{$item->deskripsi}}</td>
-                            <td>
-                                @if ($item->validasi == 0)
-                                <span class="badge bg-info"><i class="fas fa-clock"></i> Diproses</span>
-                                @elseif ($item->validasi == 1)
-                                <span class="badge bg-success">Disetujui</span>
-                                
-                                @else
-                                <span class="badge bg-danger">Ditolak</span>
-                                    
-                                @endif
-                            </td>
-                          </tr>
-                    @endforeach
-                    </tbody>
-                  </table>
-                </div>
-                <!-- /.card-body -->
-              </div>
-              <!-- /.card -->
+              </form> --}}
             </div>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body">
+            <table class="table table-hover table-striped text-nowrap table-sm">
+              <thead>
+                <tr style="font-size:11px; font-family:Arial, Helvetica, sans-serif" class="bg-gradient-primary">
+                  <th>#</th>
+                  <th>Bulan</th>
+                  <th>Tahun</th>
+                  <th>Aksi</th>
+                </tr>
+              </thead>
+              @php
+              $no =1;
+              @endphp
+              <tbody>
+
+                @foreach (bulanTahun() as $key => $item)
+                <tr style="font-size:11px; font-family:Arial, Helvetica, sans-serif">
+                  <td>{{$no++}}</td>
+                  <td>{{\Carbon\Carbon::createFromFormat('m',$item->bulan)->translatedFormat('F')}}</td>
+                  <td>{{$item->tahun}}</td>
+                  <td><a href="/puskesmas/laporan/rekap/{{$item->bulan}}/{{$item->tahun}}"
+                      class="btn btn-xs btn-success"><i class="fas fa-eye"></i> Detail</a></td>
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+            {{-- <table class="table table-hover text-nowrap table-sm">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Tanggal</th>
+                  <th>Jam</th>
+                  <th>Menit</th>
+                  <th>Aktivitas</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              @php
+              $no =1;
+              @endphp
+              <tbody>
+                @foreach ($data as $key => $item)
+                <tr style="font-size:12px; font-family:Arial, Helvetica, sans-serif">
+                  <td>{{$key+ $data->firstItem()}}</td>
+                  <td>{{\Carbon\Carbon::parse($item->tanggal)->isoFormat('D MMMM Y')}}</td>
+                  <td>{{\Carbon\Carbon::parse($item->jam_mulai)->format('H:i')}} -
+                    {{\Carbon\Carbon::parse($item->jam_selesai)->format('H:i')}}</td>
+                  <td>{{$item->menit}}</td>
+                  <td>{{$item->deskripsi}}</td>
+                  <td>
+                    @if ($item->validasi == 0)
+                    <span class="badge bg-info"><i class="fas fa-clock"></i> Diproses</span>
+                    @elseif ($item->validasi == 1)
+                    <span class="badge bg-success">Disetujui</span>
+
+                    @else
+                    <span class="badge bg-danger">Ditolak</span>
+
+                    @endif
+                  </td>
+                </tr>
+                @endforeach
+              </tbody>
+            </table> --}}
+          </div>
+          <!-- /.card-body -->
         </div>
+        <!-- /.card -->
+      </div>
     </div>
+  </div>
 </div>
 
 @endsection

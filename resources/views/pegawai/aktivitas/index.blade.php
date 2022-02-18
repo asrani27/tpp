a2dd456dfe57336a73b305cfbe3d92ed2d5fbb23
asrani <asrani.27@gmail.com>
2022-02-18 09:10:19 +0800

    f

--- resources/views/pegawai/aktivitas/index.blade.php
+++ resources/views/pegawai/aktivitas/index.blade.php
@@ -1,73 +1,79 @@
 @extends('layouts.app')
 
 @push('css')
-    
+
 @endpush
 
 @section('title')
-    JURNAL AKTIVITAS
+JURNAL AKTIVITAS
 @endsection
 @section('content')
 <div class="row">
     <div class="col-md-12">
-        
+
         <div class="row">
             <div class="col-lg-6 col-12">
                 <div class="card card-widget widget-user-2">
                     <!-- Add the bg color to the header using any of the bg-* classes -->
                     <div class="widget-user-header bg-gradient-blue">
-                      <div class="widget-user-image">
-                        <img class="img-circle elevation-2" src="/login_tpp/images/icons/logo.png" alt="User Avatar">
-                      </div>
-                      <!-- /.widget-user-image -->
-                      @if (Auth::user()->pegawai->jabatan->sekda == 1)
+                        <div class="widget-user-image">
+                            <img class="img-circle elevation-2" src="/login_tpp/images/icons/logo.png"
+                                alt="User Avatar">
+                        </div>
+                        <!-- /.widget-user-image -->
+                        @if (Auth::user()->pegawai->jabatan->sekda == 1)
                         <h3 class="widget-user-username">WALIKOTA</h3>
                         <h5 class="widget-user-desc">KOTA BANJARMASIN</h5>
-                      @else
+                        @else
                         <h3 class="widget-user-username">
                             @if ($atasan->pegawai == null)
-                                @if ($atasan->pegawaiplt == null)
-                            
-                                    @if ($atasan->pegawaiplh == null)
-                                        -
-                                    @else
-                                        {{$atasan->pegawaiplh->nama}}
-                                    @endif
-                                @else
-                                    {{$atasan->pegawaiplt->nama}}
-                                @endif
+                            @if ($atasan->pegawaiplt == null)
+
+                            @if ($atasan->pegawaiplh == null)
+                            -
                             @else
-                                {{$atasan->pegawai->nama}}
+                            {{$atasan->pegawaiplh->nama}}
                             @endif
-                        </h3>                          
-                        {{-- <h3 class="widget-user-username">{{$atasan->pegawai == null ? '-': $atasan->pegawai->nama}}</h3> --}}
-                        <h5 class="widget-user-desc">{{$atasan->pegawaiplt == null ? "":'Plt. '}} {{$atasan->pegawaiplh == null ? "":'Plh. '}}  {{$atasan->nama}}</h5>
-                      @endif
+                            @else
+                            {{$atasan->pegawaiplt->nama}}
+                            @endif
+                            @else
+                            {{$atasan->pegawai->nama}}
+                            @endif
+                        </h3>
+                        {{-- <h3 class="widget-user-username">{{$atasan->pegawai == null ? '-': $atasan->pegawai->nama}}
+                        </h3> --}}
+                        <h5 class="widget-user-desc">{{$atasan->pegawaiplt == null ? "":'Plt. '}} {{$atasan->pegawaiplh
+                            == null ? "":'Plh. '}} {{$atasan->nama}}</h5>
+                        @endif
                     </div>
-                    
+
                 </div>
             </div>
             <div class="col-lg-6 col-12">
                 <div class="card card-widget widget-user-2">
                     <!-- Add the bg color to the header using any of the bg-* classes -->
                     <div class="widget-user-header bg-gradient-purple">
-                      <div class="widget-user-image">
-                        <img class="img-circle elevation-2" src="/login_tpp/images/icons/logo.png" alt="User Avatar">
-                      </div>
-                      <!-- /.widget-user-image -->
-                      <h3 class="widget-user-username">{{$person->nama}}</h3>
-                      <h5 class="widget-user-desc">{{$person->jabatan->nama}} <br/>
-                        {{$person->jabatanPlt == null ? '': 'Plt. '.$person->jabatanPlt->nama}}
-                      </h5>
+                        <div class="widget-user-image">
+                            <img class="img-circle elevation-2" src="/login_tpp/images/icons/logo.png"
+                                alt="User Avatar">
+                        </div>
+                        <!-- /.widget-user-image -->
+                        <h3 class="widget-user-username">{{$person->nama}}</h3>
+                        <h5 class="widget-user-desc">{{$person->jabatan->nama}} <br />
+                            {{$person->jabatanPlt == null ? '': 'Plt. '.$person->jabatanPlt->nama}}
+                            {{$person->jabatanPlh == null ? '': 'Plh. '.$person->jabatanPlh->nama}}
+                        </h5>
                     </div>
-                    
+
                 </div>
             </div>
-            
+
         </div>
         <div class="row">
             <div class="col-12">
-                <a href="/pegawai/aktivitas/add" class="btn btn-success btn-block"><i class="fas fa-plus"></i> Tambah Aktivitas</a>
+                <a href="/pegawai/aktivitas/add" class="btn btn-success btn-block"><i class="fas fa-plus"></i> Tambah
+                    Aktivitas</a>
             </div>
         </div>
         <br />
@@ -76,76 +82,84 @@
                 <div class="card">
                     <div class="card-header">
                         <h3 class="card-title">Total : {{$data->total()}} Aktivitas</h3>
-        
+
                         <div class="card-tools">
-                          {{-- <form method="get" action="/pegawai/skp/rencana-kegiatan/search">
-                          <div class="input-group input-group-sm" style="width: 300px;">
-                            <input type="text" name="search" class="form-control input-sm float-right" value="{{old('search')}}" placeholder="Cari">
-        
-                            <div class="input-group-append">
-                              <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
-                            </div>
-                          </div>
-                          </form> --}}
+                            {{-- <form method="get" action="/pegawai/skp/rencana-kegiatan/search">
+                                <div class="input-group input-group-sm" style="width: 300px;">
+                                    <input type="text" name="search" class="form-control input-sm float-right"
+                                        value="{{old('search')}}" placeholder="Cari">
+
+                                    <div class="input-group-append">
+                                        <button type="submit" class="btn btn-default"><i
+                                                class="fas fa-search"></i></button>
+                                    </div>
+                                </div>
+                            </form> --}}
                         </div>
                     </div>
                     <div class="card-body table-responsive p-0">
                         <table class="table table-hover text-nowrap table-sm">
-                        <thead>
-                            <tr>
-                            <th>#</th>
-                            <th>Tanggal</th>
-                            <th>Jam</th>
-                            <th>Menit</th>
-                            <th>Aktivitas</th>
-                            <th>Status</th>
-                            <th>Di nilai Oleh</th>
-                            <th></th>
-                            </tr>
-                        </thead>
-                        @php
-                            $no =1;
-                        @endphp
-                        <tbody>
-                        @foreach ($data as $key => $item)
-                                <tr style="font-size:12px; font-family:Arial, Helvetica, sans-serif">
-                                <td>{{$key+ $data->firstItem()}}</td>
-                                <td>{{\Carbon\Carbon::parse($item->tanggal)->isoFormat('D MMMM Y')}}</td>
-                                <td>{{\Carbon\Carbon::parse($item->jam_mulai)->format('H:i')}} - {{\Carbon\Carbon::parse($item->jam_selesai)->format('H:i')}}</td>
-                                <td>{{$item->menit}}</td>
-                                <td>{!!wordwrap($item->deskripsi,100,"<br>")!!}</td>
-                                <td>
-                                    @if ($item->validasi == 0)
-                                    <span class="badge bg-info"><i class="fas fa-clock"></i> Diproses</span>
-                                    @elseif ($item->validasi == 1)
-                                    <span class="badge bg-success"><i class="fas fa-check"></i> Disetujui</span>
-                                    
-                                    @else
-                                    <span class="badge bg-danger"><i class="fas fa-times"></i> Ditolak</span>
-                                        
-                                    @endif
-                                </td>
-                                <td>{{$item->validator == null ? null : $item->penilai->nama}}</td>
-                                <td>
-                                    @if ($item->validasi == 0)
-                                    <a href="/pegawai/aktivitas/harian/edit/{{$item->id}}" class="btn btn-xs btn-success text-white" data-toggle="tooltip" title="edit data"><i class="fas fa-edit"></i></a>
-                                    <a href="/pegawai/aktivitas/harian/delete/{{$item->id}}" class="btn btn-xs btn-danger text-white" data-toggle="tooltip" title="hapus data"  onclick="return confirm('Yakin ingin di hapus?');"><i class="fas fa-trash"></i></a>
-                                    @else
-                                    
-                                    @endif
-                                </td>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Tanggal</th>
+                                    <th>Jam</th>
+                                    <th>Menit</th>
+                                    <th>Aktivitas</th>
+                                    <th>Status</th>
+                                    <th>Di nilai Oleh</th>
+                                    <th></th>
                                 </tr>
-                        @endforeach
-                        </tbody>
+                            </thead>
+                            @php
+                            $no =1;
+                            @endphp
+                            <tbody>
+                                @foreach ($data as $key => $item)
+                                <tr style="font-size:12px; font-family:Arial, Helvetica, sans-serif">
+                                    <td>{{$key+ $data->firstItem()}}</td>
+                                    <td>{{\Carbon\Carbon::parse($item->tanggal)->isoFormat('D MMMM Y')}}</td>
+                                    <td>{{\Carbon\Carbon::parse($item->jam_mulai)->format('H:i')}} -
+                                        {{\Carbon\Carbon::parse($item->jam_selesai)->format('H:i')}}</td>
+                                    <td>{{$item->menit}}</td>
+                                    <td>{!!wordwrap($item->deskripsi,100,"<br>")!!}</td>
+                                    <td>
+                                        @if ($item->validasi == 0)
+                                        <span class="badge bg-info"><i class="fas fa-clock"></i> Diproses</span>
+                                        @elseif ($item->validasi == 1)
+                                        <span class="badge bg-success"><i class="fas fa-check"></i> Disetujui</span>
+
+                                        @else
+                                        <span class="badge bg-danger"><i class="fas fa-times"></i> Ditolak</span>
+
+                                        @endif
+                                    </td>
+                                    <td>{{$item->validator == null ? null : $item->penilai->nama}}</td>
+                                    <td>
+                                        @if ($item->validasi == 0)
+                                        <a href="/pegawai/aktivitas/harian/edit/{{$item->id}}"
+                                            class="btn btn-xs btn-success text-white" data-toggle="tooltip"
+                                            title="edit data"><i class="fas fa-edit"></i></a>
+                                        <a href="/pegawai/aktivitas/harian/delete/{{$item->id}}"
+                                            class="btn btn-xs btn-danger text-white" data-toggle="tooltip"
+                                            title="hapus data" onclick="return confirm('Yakin ingin di hapus?');"><i
+                                                class="fas fa-trash"></i></a>
+                                        @else
+
+                                        @endif
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
                         </table>
                     </div>
                 </div>
-                
+
                 {{$data->links()}}
             </div>
         </div>
         <br />
-        
+
     </div>
 </div>
 @endsection

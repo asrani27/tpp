103991fb90474e0a5e39e3034edf10995a2a309b
asrani <asrani.27@gmail.com>
2022-03-14 08:28:35 +0800

    f

--- app/Http/Controllers/RekapitulasiController.php
+++ app/Http/Controllers/RekapitulasiController.php
@@ -486,7 +486,7 @@ class RekapitulasiController extends Controller
     public function uploadBpjs(Request $req, $bulan, $tahun)
     {
         $validator = Validator::make($req->all(), [
-            'file' => 'required|mimes:xlx,xls',
+            'file' => 'mimes:xlx,xls',
         ]);
 
         if ($validator->fails()) {

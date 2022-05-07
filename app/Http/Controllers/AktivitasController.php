1a0956bbd161710c6b2ed4659e6ae2a9a26cce01
asrani <asrani.27@gmail.com>
2022-05-07 22:41:17 +0800

    f

--- app/Http/Controllers/AktivitasController.php
+++ app/Http/Controllers/AktivitasController.php
@@ -23,6 +23,10 @@ class AktivitasController extends Controller
 
     public function index()
     {
+        if ($this->user()->change_password == 0) {
+            return redirect('/pegawai/gantipass');
+        }
+
         if ($this->user()->pegawai->jabatan == null) {
             toastr()->info('Tidak bisa melakukan aktivitas, Karena Tidak memiliki jabatan,untuk melihat riwayat silahkan ke menu laporan aktivitas');
             return back();

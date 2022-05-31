4e7414df3f512566a6c8f36a5fe49d0ccc37c3ef
asrani <asrani.27@gmail.com>
2022-05-31 19:04:48 +0800

    f

--- app/Http/Controllers/API/PegawaiController.php
+++ app/Http/Controllers/API/PegawaiController.php
@@ -12,24 +12,23 @@ class PegawaiController extends Controller
 
     public function allpegawai()
     {
-        $pegawai = Pegawai::with('jabatan','pangkat')->get();
+        $pegawai = Pegawai::with('jabatan', 'pangkat')->get();
 
         $data['message_error'] = 200;
         $data['message']       = 'data ditemukan';
         $data['data']          = $pegawai;
         $data['jumlah']        = count($pegawai);
         return response()->json($data);
-
     }
     public function pegawai($nip)
     {
-        $pegawai = Pegawai::with('jabatan','pangkat')->where('nip', $nip)->first();
-        if($pegawai == null){
+        $pegawai = Pegawai::with('jabatan', 'pangkat')->where('nip', $nip)->first();
+        if ($pegawai == null) {
             $data['message_error'] = 101;
             $data['message']       = 'username atau password kosong';
             $data['data']          = null;
             return response()->json($data);
-        }else{
+        } else {
             $data['message_error'] = 200;
             $data['message']       = 'data ditemukan';
             $data['data']          = $pegawai;
@@ -39,8 +38,10 @@ class PegawaiController extends Controller
 
     public function pegawaiSkpd($id)
     {
+
+        return response()->json($id);
         $skpd_id = Skpd::where('kode_skpd', $id)->first()->id;
-        $pegawai = Pegawai::with('jabatan','pangkat')->where('skpd_id', $skpd_id)->get();
+        $pegawai = Pegawai::with('jabatan', 'pangkat')->where('skpd_id', $skpd_id)->get();
         $data['message_error'] = 200;
         $data['message']       = 'data ditemukan';
         $data['data']          = $pegawai;

e3fdd16591fd31180dc1d53c03f7109bc18bd50c
asrani <asrani.27@gmail.com>
2022-06-02 14:20:47 +0800

    f

--- app/Http/Controllers/API/PegawaiController.php
+++ app/Http/Controllers/API/PegawaiController.php
@@ -36,6 +36,24 @@ class PegawaiController extends Controller
         }
     }
 
+    public function aktivitaspegawai($nip, $bulan, $tahun)
+    {
+        $pegawai = Pegawai::where('nip', $nip)->first();
+        if ($pegawai == null) {
+            $data['message_error'] = 101;
+            $data['message']       = 'username atau password kosong';
+            $data['data']          = null;
+            return response()->json($data);
+        } else {
+            $aktivitas = Aktivitas::where('pegawai_id', $pegawai->id)->whereMonth('tanggal', $bulan)->whereYear('tanggal', $tahun)->get();
+            $data['message_error'] = 200;
+            $data['message']       = 'data ditemukan';
+            $data['ja']          = $aktivitas->count();
+            $data['ma']          = $aktivitas->sum('menit');
+            return response()->json($data);
+        }
+    }
+
     public function pegawaiSkpd($id)
     {
         $skpd_id = Skpd::where('kode_skpd', $id)->first()->id;
--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', 'API\LoginController@login');
 Route::get('/pegawai', 'API\PegawaiController@allpegawai');
 Route::get('/pegawai/{nip}', 'API\PegawaiController@pegawai');
+Route::get('/pegawai/aktivitas/{nip}/{bulan}/{tahun}', 'API\PegawaiController@aktivitaspegawai');
 Route::get('/pegawai/skpd/{id}', 'API\PegawaiController@pegawaiSkpd');
 Route::get('/skpd', 'API\SkpdController@all');
 // Route::middleware('auth:api')->get('/user', function (Request $request) {

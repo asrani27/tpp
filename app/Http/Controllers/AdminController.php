4bde48c55bd8de0c4b394b809f71ddd7fcce2eb0
asrani <asrani.27@gmail.com>
2022-06-03 08:30:50 +0800

    f

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -294,7 +294,7 @@ class AdminController extends Controller
 
     public function editPersen()
     {
-        $data = Jabatan::where('skpd_id', Auth::user()->skpd->id)->where('rs_puskesmas_id', null)->orderBy('kelas_id', 'DESC')->get();
+        $data = Jabatan::where('skpd_id', Auth::user()->skpd->id)->orderBy('kelas_id', 'DESC')->get();
         return view('admin.edit_persen', compact('data'));
     }
 

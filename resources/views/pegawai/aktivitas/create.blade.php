8c2ee9897d8b3d348ea6c18266d17a81c83a7953
asrani <asrani.27@gmail.com>
2022-07-08 04:36:37 +0800

    f

--- resources/views/pegawai/aktivitas/create.blade.php
+++ resources/views/pegawai/aktivitas/create.blade.php
@@ -32,7 +32,7 @@ TAMBAH AKTIVITAS HARIAN
           <div class="form-group row">
             <label class="col-sm-2 col-form-label">Tanggal</label>
             <div class="col-sm-10">
-
+              {{--
               @if (Auth::user()->username == '198806042007011001')
               <input type="date" class="form-control" name="tanggal" placeholder="" value="{{$tanggal}}">
               @else
@@ -40,7 +40,11 @@ TAMBAH AKTIVITAS HARIAN
               <input type="date" class="form-control" name="tanggal" placeholder="" value="{{$tanggal}}"
                 min="{{\Carbon\Carbon::today()->subdays(1)->format('Y-m-d')}}"
                 max="{{\Carbon\Carbon::today()->format('Y-m-d')}}">
-              @endif
+              @endif --}}
+
+              <input type="date" class="form-control" name="tanggal" placeholder="" value="{{$tanggal}}"
+                min="{{\Carbon\Carbon::today()->subdays(1)->format('Y-m-d')}}"
+                max="{{\Carbon\Carbon::today()->format('Y-m-d')}}">
             </div>
           </div>
           <div class="form-group row">

a27505e32859384e37cf3389a0dcb85b95699930
asrani <asrani.27@gmail.com>
2022-08-12 16:10:05 +0800

    f

--- resources/views/pegawai/aktivitas/create.blade.php
+++ resources/views/pegawai/aktivitas/create.blade.php
@@ -33,18 +33,19 @@ TAMBAH AKTIVITAS HARIAN
             <label class="col-sm-2 col-form-label">Tanggal</label>
             <div class="col-sm-10">
 
-              {{-- @if (Auth::user()->username == '197211302005011005')
-              <input type="date" class="form-control" name="tanggal" placeholder="" value="{{$tanggal}}">
+              @if (Auth::user()->username == '198707242010011009')
+              <input type="date" class="form-control" name="tanggal" placeholder="" value="{{$tanggal}}"
+                max="{{\Carbon\Carbon::today()->format('Y-m-d')}}">
               @else
 
               <input type="date" class="form-control" name="tanggal" placeholder="" value="{{$tanggal}}"
                 min="{{\Carbon\Carbon::today()->subdays(1)->format('Y-m-d')}}"
                 max="{{\Carbon\Carbon::today()->format('Y-m-d')}}">
-              @endif --}}
+              @endif
 
-              <input type="date" class="form-control" name="tanggal" placeholder="" value="{{$tanggal}}"
+              {{-- <input type="date" class="form-control" name="tanggal" placeholder="" value="{{$tanggal}}"
                 min="{{\Carbon\Carbon::today()->subdays(1)->format('Y-m-d')}}"
-                max="{{\Carbon\Carbon::today()->format('Y-m-d')}}">
+                max="{{\Carbon\Carbon::today()->format('Y-m-d')}}"> --}}
             </div>
           </div>
           <div class="form-group row">

d0bf34f3961b8b7feab419ddbf8230339628f67f
asrani <asrani.27@gmail.com>
2022-10-04 10:11:34 +0800

    f

--- app/Http/Controllers/PuskesmasController.php
+++ app/Http/Controllers/PuskesmasController.php
@@ -119,6 +119,7 @@ class PuskesmasController extends Controller
                 if (Auth::user()->puskesmas->id == 8) {
 
                     $pagu_asn  = ($disiplin + $produktivitas + $kondisi_kerja + $tambahan_beban_kerja + $kelangkaan_profesi) * 0.7;
+                    dd($pagu_asn, $disiplin, $produktivitas, $kondisi_kerja, $tambahan_beban_kerja, $kelangkaan_profesi, $item);
                 } else {
                     $pagu_asn  = $disiplin + $produktivitas + $kondisi_kerja + $tambahan_beban_kerja + $kelangkaan_profesi;
                 }

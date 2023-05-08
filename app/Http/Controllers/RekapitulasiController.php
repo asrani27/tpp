df6f12b736f984ec355a1921db127e70d0113f45
asrani <asrani.27@gmail.com>
2023-05-08 08:55:08 +0800

    f

--- app/Http/Controllers/RekapitulasiController.php
+++ app/Http/Controllers/RekapitulasiController.php
@@ -1232,7 +1232,9 @@ class RekapitulasiController extends Controller
             $item->pbk_absensi = $item->basic * (($item->p_bk + $item->p_tbk) / 100) * (40 / 100) * ($item->dp_absensi / 100);
             if ($item->dp_ta >= 6750) {
                 $item->pbk_aktivitas = $item->basic * (($item->p_bk + $item->p_tbk) / 100) * (40 / 100);
-                if ($item->dp_skp == 'kurang') {
+                if ($item->dp_skp == null) {
+                    $item->pbk_skp = 0;
+                } else if ($item->dp_skp == 'KURANG' || $item->dp_skp == "SANGAT KURANG") {
                     $item->pbk_skp = $item->basic * (($item->p_bk + $item->p_tbk) / 100) * (10 / 100);
                 } else {
                     $item->pbk_skp = $item->basic * (($item->p_bk + $item->p_tbk) / 100) * (20 / 100);
@@ -1247,7 +1249,9 @@ class RekapitulasiController extends Controller
             $item->ppk_absensi = $item->basic * ($item->p_pk / 100) * (40 / 100) * ($item->dp_absensi / 100);
             if ($item->dp_ta >= 6750) {
                 $item->ppk_aktivitas = $item->basic * ($item->p_pk / 100) * (40 / 100);
-                if ($item->dp_skp == 'kurang') {
+                if ($item->dp_skp == null) {
+                    $item->pbk_skp = 0;
+                } else if ($item->dp_skp == 'KURANG' || $item->dp_skp == "SANGAT KURANG") {
                     $item->ppk_skp = $item->basic * ($item->p_pk / 100) * (10 / 100);
                 } else {
                     $item->ppk_skp = $item->basic * ($item->p_pk / 100) * (20 / 100);

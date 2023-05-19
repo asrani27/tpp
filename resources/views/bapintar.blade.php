dacd50fa4a05a28f56fd45af4dd8cb72095eb1c4
asrani <asrani.27@gmail.com>
2023-05-19 09:24:23 +0800

    f

--- resources/views/bapintar.blade.php
+++ resources/views/bapintar.blade.php
@@ -10,8 +10,8 @@
 <div class="row">
   <div class="col-md-12 text-center">
     <h2>DAFTAR BAPINTAR TERLEBIH DAHULU SEBELUM ISI AKTIVITAS</h2>
-    Pengguna Android bisa download dan install di <a href="https://play.google.com/store/apps/details?id=com.banjarmasinkota.app_bapintar_diskominfotik&hl=id">https://play.google.com/store/apps/details?id=com.banjarmasinkota.app_bapintar_diskominfotik&hl=id</a><br/>
-    Pengguna Iphone dan Komputer bisa daftar di link <a href="https://bapintar.banjarmasinkota.go.id/register">https://bapintar.banjarmasinkota.go.id/register</a><br/>
+    {{-- Pengguna Android bisa download dan install di <a href="https://play.google.com/store/apps/details?id=com.banjarmasinkota.app_bapintar_diskominfotik&hl=id">https://play.google.com/store/apps/details?id=com.banjarmasinkota.app_bapintar_diskominfotik&hl=id</a><br/> --}}
+    Silahkah Daftar Di Link ini <a href="https://bapintar.banjarmasinkota.go.id/register">https://bapintar.banjarmasinkota.go.id/register</a><br/>
     <img src="/logo/bapintar.png" width="30%" height="250px">
     <h3>SUDAH DAFTAR DI BANJARMASIN PINTAR ? <BR/>SILAHKAN ISI EMAIL YANG TELAH DI DAFTARKAN DI BANJARMASIN PINTAR DAN KLIK CHECK :</h3>
    

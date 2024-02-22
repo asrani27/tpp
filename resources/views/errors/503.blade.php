380f967656a6ba22e340153d340efc10b8292580
asrani <asrani.27@gmail.com>
2024-02-22 11:15:43 +0800

    f

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -32,12 +32,15 @@
 			</h3>
 
 			<p class="txt-center l1-txt2 p-b-60">
-				Website Sedang Kembali Ke Pengaturan pYang Lama
+				Website Sedang Dalam Perbaikan
 			</p>
 
-
 			<button class="flex-c-m s1-txt2 size3 how-btn"  data-toggle="modal" data-target="#subscribe">
-				Aktif Kembali Pada Pukul 16:00 WITA
+				KAMI AKAN SEGERA KEMBALI
+			</button><br/>
+
+			<button class="flex-c-m s1-txt2 size3 how-btn"  id="demo" data-toggle="modal" data-target="#subscribe">
+				
 			</button>
 		</div>
 
@@ -73,5 +76,36 @@
 <!--===============================================================================================-->
 	<script src="/errors/503/js/main.js"></script>
 
+
+	<script>
+		// Set the date we're counting down to
+		var countDownDate = new Date("Feb 22, 2024 13:37:25").getTime();
+		
+		// Update the count down every 1 second
+		var x = setInterval(function() {
+		
+		  // Get today's date and time
+		  var now = new Date().getTime();
+			
+		  // Find the distance between now and the count down date
+		  var distance = countDownDate - now;
+			
+		  // Time calculations for days, hours, minutes and seconds
+		  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+			
+		  // Output the result in an element with id="demo"
+		  document.getElementById("demo").innerHTML = hours + " Jam "
+		  + minutes + " Menit " + seconds + " Detik ";
+			
+		  // If the count down is over, write some text 
+		  if (distance < 0) {
+			clearInterval(x);
+			document.getElementById("demo").innerHTML = "EXPIRED";
+		  }
+		}, 1000);
+		</script>
 </body>
 </html>

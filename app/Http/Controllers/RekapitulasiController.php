b00ddf845e2d58b32a25b610bb5f8d9d4cba0690
asrani <asrani.27@gmail.com>
2025-01-16 17:00:41 +0800

    f

--- app/Http/Controllers/RekapitulasiController.php
+++ app/Http/Controllers/RekapitulasiController.php
@@ -2350,7 +2350,7 @@ class RekapitulasiController extends Controller
             $spreadsheet->getSheetByName('REGULER')->setCellValue('O' . $contentRow, ($item->dp_absensi / 100));
             $spreadsheet->getSheetByName('REGULER')->setCellValue('P' . $contentRow, $item->dp_ta);
             $spreadsheet->getSheetByName('REGULER')->setCellValue('Q' . $contentRow, $item->dp_skp);
-            $spreadsheet->getSheetByName('REGULER')->setCellValue('R' . $contentRow, ($item->pph_terutang));
+            $spreadsheet->getSheetByName('REGULER')->setCellValue('R' . $contentRow, $item->pph_terutang);
             $spreadsheet->getSheetByName('REGULER')->setCellValue('AG' . $contentRow, $item->bpjs1);
             $spreadsheet->getSheetByName('REGULER')->setCellValue('AH' . $contentRow, $item->bpjs4);
             $contentRow++;

a1df8d0b387ca94b22bf5537248ac0f48003cde1
asrani <asrani.27@gmail.com>
2025-02-05 11:44:56 +0800

    f

--- resources/views/pegawai/aktivitas/create.blade.php
+++ resources/views/pegawai/aktivitas/create.blade.php
@@ -33,8 +33,7 @@ TAMBAH AKTIVITAS HARIAN
             <label class="col-sm-2 col-form-label">Tanggal</label>
             <div class="col-sm-10">
 
-              @if (Auth::user()->username == '197508312010011005' || Auth::user()->username == '198609142011012004' ||
-              Auth::user()->username == '198707242010011009' || Auth::user()->username == '197410251995061001')
+              @if (Auth::user()->username == '197508312010011005')
 
               <input type="date" class="form-control" name="tanggal" placeholder="" value="{{$tanggal}}"
                 max="{{\Carbon\Carbon::today()->format('Y-m-d')}}">

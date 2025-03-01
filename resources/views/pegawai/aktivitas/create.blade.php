027f857f7acb1b24e3da30ba178235eec72956cb
asrani <asrani.27@gmail.com>
2025-03-02 04:33:45 +0800

    f

--- resources/views/pegawai/aktivitas/create.blade.php
+++ resources/views/pegawai/aktivitas/create.blade.php
@@ -35,8 +35,7 @@ TAMBAH AKTIVITAS HARIAN
 
               @if (Auth::user()->username == '197508312010011005'||Auth::user()->username ==
               '198707242010011009'||Auth::user()->username == '197901192011012002'||Auth::user()->username ==
-              '198902192015021002'||Auth::user()->username ==
-              '198708132010011001'||Auth::user()->username ==
+              '198902192015021002'||Auth::user()->username == '198708132010011001'||Auth::user()->username ==
               '199612132019031004')
 
               <input type="date" class="form-control" name="tanggal" placeholder="" value="{{$tanggal}}"

b16a9b462a75344ad1404295517739b07631299c
asrani <asrani.27@gmail.com>
2025-03-05 14:39:42 +0800

    f

--- resources/views/pegawai/aktivitas/create.blade.php
+++ resources/views/pegawai/aktivitas/create.blade.php
@@ -34,10 +34,7 @@ TAMBAH AKTIVITAS HARIAN
             <div class="col-sm-10">
 
               @if (Auth::user()->username == '197508312010011005'||Auth::user()->username ==
-              '198707242010011009'||Auth::user()->username == '196906081997032006'||Auth::user()->username ==
-              '199612132019031004'||Auth::user()->username ==
-              '197911032009041001'||Auth::user()->username ==
-              '199212122020122019')
+              '198707242010011009'||Auth::user()->username == '198311052010012001')
 
               <input type="date" class="form-control" name="tanggal" placeholder="" value="{{$tanggal}}"
                 max="{{\Carbon\Carbon::today()->format('Y-m-d')}}">

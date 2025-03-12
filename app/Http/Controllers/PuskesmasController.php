c2e2275a3862abea4d2286a8a53c03ffeb9cfa5a
asrani <asrani.27@gmail.com>
2025-03-12 09:45:14 +0800

    f

--- app/Http/Controllers/PuskesmasController.php
+++ app/Http/Controllers/PuskesmasController.php
@@ -659,7 +659,12 @@ class PuskesmasController extends Controller
                 $p_pk      = $persen->persen_prestasi_kerja;
                 $p_kk      = $persen->persen_kondisi_kerja;
                 $p_kp      = $persen->persen_kelangkaan_profesi;
-                $pagu      = round($basic * (($p_bk + $p_tbk + $p_pk + $p_kk + $p_kp) / 100) * (85 / 100));
+
+                if (Auth::user()->puskesmas->id == 8) {
+                    $pagu      = round($basic * (($p_bk + $p_tbk + $p_pk + $p_kk + $p_kp) / 100) * (85 / 100));
+                } else {
+                    $pagu      = round($basic * (($p_bk + $p_tbk + $p_pk + $p_kk + $p_kp) / 100) * (85 / 100));
+                }
             }
 
             $item->update([
